78ee08172f027af8e9ed3ef5e84e69eb3809810b
Oscar Metten <oscar.metten@gmail.com>
2021-03-15 16:14:10 +0100

    menu principal et patrouille

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    public void Onclick_Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Onclick_Exit()
+    {
+        Application.Quit(0);
+    }
+
+    public void Onclick_MuteSounds()
+    {
+        AudioListener.pause = true;
+    }
+}
