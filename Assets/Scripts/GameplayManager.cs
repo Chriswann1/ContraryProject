8417f83046bbf5f4275bb170c6391119ca6f5dc1
Chriswann <chriswann@gmail.com>
2021-03-15 15:32:27 +0100

    GameplayManager + savewaypoint

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour
+{
+    
+    public Vector2 lastwaypoint;
+    
+    private GameObject player;
+    public static GameplayManager Instance; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        
+    }
+}
