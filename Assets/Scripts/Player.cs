7b25b287f863b079a93f8ddb3af982a1cb3855bc
Oscar Metten <oscar.metten@gmail.com>
2021-03-16 14:39:05 +0100

    merge correctif

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,12 +21,7 @@ public class Player : MonoBehaviour
         
         transform.Translate(input.normalized * Time.deltaTime * speed,0f);
         
-<<<<<<< HEAD
         if (input.x >= 1 && Time.timeScale != 0)
-=======
-        
-        if (input.x >= 1)
->>>>>>> main
         { chickenrotate.transform.rotation = Quaternion.Euler(0,0,-90); }
         if (input.x <= -1 && Time.timeScale != 0)
         { chickenrotate.transform.rotation = Quaternion.Euler(0,0,90); }
