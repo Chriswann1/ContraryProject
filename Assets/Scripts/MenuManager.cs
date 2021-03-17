0de1020b5a6edb25ff312dfdc65d29b9337a778d
Tanneau <tanneauthomas56@gmail.com>
2021-03-17 17:11:00 +0100

    MainMenuModif

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,17 +5,20 @@ public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance;
 
-    public void Onclick_Play()
+    /*public void Onclick_Play()
     {
         SceneManager.LoadScene(1);
+    }*/
+    public void OnClickLevel(int level)
+    {
+        SceneManager.LoadScene(level);
     }
     public void Onclick_Exit()
     {
         Application.Quit(0);
     }
-
     public void Onclick_MuteSounds()
     {
-        AudioListener.pause = true;
+        AudioListener.pause = !AudioListener.pause;
     }
 }
