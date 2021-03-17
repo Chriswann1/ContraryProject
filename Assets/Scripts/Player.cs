76e1add942e3c064203584f028b55eadb2527e2c
Romain.C <romain130197@gmail.com>
2021-03-17 11:13:43 +0100

    Tomb + canMove

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,21 +16,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region Move
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
-        transform.Translate(input.normalized * Time.deltaTime * speed,0f);
-        
-        if (input.x >= 1 && Time.timeScale != 0)
-        { chickenrotate.transform.rotation = Quaternion.Euler(0,0,-90); }
-        if (input.x <= -1 && Time.timeScale != 0)
-        { chickenrotate.transform.rotation = Quaternion.Euler(0,0,90); }
-        if (input.y >= 1 && Time.timeScale != 0)
-        { chickenrotate.transform.rotation = Quaternion.Euler(0,0,0); }
-        if (input.y <= -1 && Time.timeScale != 0)
-        { chickenrotate.transform.rotation = Quaternion.Euler(0,0,180); }
-        #endregion
-        
+        if (GameplayManager.Instance.canMove == true)
+        {
+            #region Move
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            transform.Translate(input.normalized * Time.deltaTime * speed, 0f);
+
+            if (input.x >= 1 && Time.timeScale != 0)
+            { chickenrotate.transform.rotation = Quaternion.Euler(0, 0, -90); }
+            if (input.x <= -1 && Time.timeScale != 0)
+            { chickenrotate.transform.rotation = Quaternion.Euler(0, 0, 90); }
+            if (input.y >= 1 && Time.timeScale != 0)
+            { chickenrotate.transform.rotation = Quaternion.Euler(0, 0, 0); }
+            if (input.y <= -1 && Time.timeScale != 0)
+            { chickenrotate.transform.rotation = Quaternion.Euler(0, 0, 180); }
+            #endregion
+        }
+
         if (Input.GetKeyDown(KeyCode.C))
         {
             audiochicken.Play();
